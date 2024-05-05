8b15d190904aec6ba39632ce47003864bbec106f
Pedro Yanes Garrido <pedro.yanes@edu.uah.es>
2024-05-06 00:35:03 +0200

    json file fixed

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -9,6 +9,12 @@ const params = {
 
 export async function GET(request) {
   const auth = new GoogleAuth({
+    credentials: {
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      project_id: process.env.GOOGLE_PROJECT_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY      
+    },
     scopes: "https://www.googleapis.com/auth/cloud-platform",
   });
   const client = await auth.getClient();
