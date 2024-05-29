420bf977ef691305203dec57c3d94e6e3642ec1c
Pedro Yanes Garrido <pedro.yanes@edu.uah.es>
2024-05-29 02:04:34 +0200

    fix

--- src/app/metrics.js
+++ src/app/metrics.js
@@ -1,29 +1,29 @@
-const client = require('prom-client');
+import { Registry, Histogram, Counter } from 'prom-client';
 
 // Crea un nuevo Registro
-const register = new client.Registry();
+const register = new Registry();
 
 // Crea un contador de ejemplo
-const counter = new client.Counter({
+const counter = new Counter({
   name: 'Peticion_API_WordPress',
   help: 'Este es un contador para contar las peticiones a la API de WordPress',
   registers: [register],
 });
 
-const duration = new client.Histogram({
+const duration = new Histogram({
   name: 'Duracion_Peticion_WordPress',
   help: 'Este es un histograma para medir la duracion de las peticiones a la API de WordPress',
   registers: [register],
   buckets: [5, 50, 500, 2500],
 });
 
-const counterAI = new client.Counter({
+const counterAI = new Counter({
   name: 'Peticion_API_Vertex_AI',
   help: 'Este es un contador para contar las peticiones a la API de Vertex AI',
   registers: [register],
 });
 
-const durationAI = new client.Histogram({
+const durationAI = new Histogram({
   name: 'Duracion_Peticion_Vertex_AI',
   help: 'Este es un histograma para medir la duracion de las peticiones a la API de Vertex AI',
   registers: [register],
