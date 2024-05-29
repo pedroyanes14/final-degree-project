63b4f491c95b6a76dc062281cbd316afcf6ebe9e
Pedro Yanes Garrido <pedro.yanes@edu.uah.es>
2024-05-30 00:03:55 +0200

    [postId] fixed

--- src/app/blog/[postId]/page.js
+++ src/app/blog/[postId]/page.js
@@ -1,17 +1,8 @@
+"use client";
+
 import { Analytics } from '@vercel/analytics/react';
-
-export const dynamicParams = true;
-
-export async function generateStaticParams() {
-	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/posts?${new Date().getTime()}`
-	);
-	const posts = await response.json();
-
-	return posts.map((post) => ({
-		postId: post.id.toString(),
-	}));
-}
+import { useState, useEffect } from 'react';
+import ContentLoader from 'react-content-loader';
 
 async function getSinglePost(postId) {
 	const response = await fetch(
@@ -21,11 +12,29 @@ async function getSinglePost(postId) {
 	return post;
 }
 
-const page = async ({ params }) => {
-	const post = await getSinglePost(params.postId);
+const Page = ({ params }) => {
+	const [post, setPost] = useState(null);
+	const [loadingPosts, setLoading] = useState(true);
 
-	if (!post) {
-		return <div>Loading...</div>;
+	useEffect(() => {
+		const fetchPost = async () => {
+		  const postData = await getSinglePost(params.postId);
+		  setPost(postData);
+		  setLoading(false);
+		};
+	
+		fetchPost();
+	}, [params.postId]);
+
+	if (loadingPosts) {
+		return <ContentLoader viewBox="0 0 2000 720" height={720} width={2000}>
+		  <rect x="0" y="13" rx="4" ry="4" width="1200" height="9" />
+		  <rect x="0" y="29" rx="4" ry="4" width="400" height="8" />
+		  <rect x="0" y="50" rx="4" ry="4" width="1200" height="10" />
+		  <rect x="0" y="65" rx="4" ry="4" width="1200" height="10" />
+		  <rect x="0" y="79" rx="4" ry="4" width="400" height="10" />
+		  <rect x="0" y="99" rx="5" ry="5" width="1200" height="200" />
+		</ContentLoader>;
 	}
 
 	return (
@@ -39,4 +48,4 @@ const page = async ({ params }) => {
 	);
 };
 
-export default page;
+export default Page;
