cb25f32878c5f60b0547a936bbfb464bcf67e4e2
Pedro Yanes Garrido <pedro.yanes@edu.uah.es>
2024-06-12 19:10:34 +0200

    locust v2

--- locustfile.py
+++ locustfile.py
@@ -4,5 +4,19 @@ class WebsiteUser(HttpUser):
     # wait_time = between(1, 5)
 
     @task
-    def metrics(self):
+    def spain(self):
+        self.client.get("/")
+        self.client.get("/blog?country=spain")
         self.client.get("/api?country=spain")
+
+    @task
+    def france(self):
+        self.client.get("/")
+        self.client.get("/blog?country=france")
+        self.client.get("/api?country=france")
+
+    @task
+    def croatia(self):
+        self.client.get("/")
+        self.client.get("/blog?country=croatia")
+        self.client.get("/api?country=croatia")
