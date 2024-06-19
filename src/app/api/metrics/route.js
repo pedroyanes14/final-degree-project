3bb79b4001cc6579832dbabcff53e6fff3ac609c
Pedro Yanes Garrido <pedro.yanes@edu.uah.es>
2024-06-19 19:33:10 +0200

    /api/metrics

--- src/app/api/metrics/route.js
+++ src/app/api/metrics/route.js
@@ -4,14 +4,12 @@ import { register } from '../../metrics';
 
 export async function GET(request) {
     const metrics = await register.metrics();
+    console.log(metrics);
     const response = new NextResponse(metrics, {
         status: 200,
         headers: { 
             'Content-Type': register.contentType,
-            'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-            'Pragma': 'no-cache',
-            'Expires': '0',
-            'Surrogate-Control': 'no-store',
+            'Cache-Control': 'no-store',
         },
     });
     return response;
