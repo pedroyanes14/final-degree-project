701d9657b8b847dbd5311f2e2a8cbcc263fb374c
Pedro Yanes Garrido <pedro.yanes@edu.uah.es>
2024-06-19 18:32:20 +0200

    logs

--- src/components/postAI.jsx
+++ src/components/postAI.jsx
@@ -4,17 +4,16 @@ import { counterAI, durationAI } from '../app/metrics';
 
 export default async function PostAI({ searchParams }) {
     const start = performance.now();
-    console.log("hola");
     const response = await fetch(
         `${process.env.NEXT_PUBLIC_BASE_API_URL ? `${process.env.NEXT_PUBLIC_BASE_API_URL}/api` : "http://localhost:3000/api"}?country=${searchParams}`
       );
     const data = await response.json();
     const duracion = performance.now() - start;
 
+    console.log(duracion);
+
     counterAI.inc();
     durationAI.set(duracion);
-
-    console.log("adios");
 
     return (
         <Link href={`/blog/vertexAI?country=${searchParams}`} className="post">
