b4c43a741f07b14378e59b1ad4bc3a469982cd9c
Pedro Yanes Garrido <pedro.yanes@edu.uah.es>
2024-06-21 00:02:28 +0200

    next config updated

--- next.config.js
+++ next.config.js
@@ -2,18 +2,7 @@
 
 module.exports = {
   experimental: {
-    /* instrumentationHooks: true,
-    serverComponentsExternalPackages: [
-      '@opentelemetry/instrumentation',
-    ],*/
     missingSuspenseWithCSRBailout: false,
   },
-  /* async rewrites() {
-    return [
-      {
-        source: '/metrics',
-        destination: '/api/metrics',
-      },
-    ];
-  }, */
+  output: 'standalone'
 };
