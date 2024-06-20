4ca299bed8367e9659ed8d817f51825914438b59
Pedro Yanes Garrido <pedro.yanes@edu.uah.es>
2024-06-20 23:33:43 +0200

    final version

--- src/app/api/metrics/route.js
+++ src/app/api/metrics/route.js
@@ -1,10 +1,11 @@
 export const dynamic = 'force-dynamic';
 import { NextResponse } from 'next/server';
 // import { client, counter, duration, counterAI, durationAI } from '../../metrics';
-import { client, duration, durationAI } from '../../metrics';
+// import { client, duration, durationAI } from '../../metrics';
+import { client } from '../../metrics';
 
 export async function GET(req) {
-    const { searchParams } = new URL(req.url);
+    /* const { searchParams } = new URL(req.url);
     const duracion = parseFloat(searchParams.get('duration'));
     const action = searchParams.get('action');
 
@@ -14,7 +15,7 @@ export async function GET(req) {
     } else if (action === 'fetchAI') {
         // counterAI.inc();
         durationAI.set(duracion);
-    }
+    } */
     const metrics = await client.register.metrics();
     const response = new NextResponse(metrics, {
         status: 200,
--- src/components/postAI.jsx
+++ src/components/postAI.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import ReactMarkdown from 'react-markdown';
-// import { client, counterAI, durationAI } from '../app/metrics';
+import { counterAI, durationAI } from '../app/metrics';
 
 export default async function PostAI({ searchParams }) {
     const start = performance.now();
@@ -10,12 +10,13 @@ export default async function PostAI({ searchParams }) {
     const data = await response.json();
     const duracion = performance.now() - start;
 
-    await fetch(
+    /* await fetch(
         `${process.env.NEXT_PUBLIC_BASE_API_URL ? `${process.env.NEXT_PUBLIC_BASE_API_URL}/api/metrics` : "http://localhost:3000/api/metrics"}?duration=${duracion}&action=fetchAI&${new Date().getTime()}`
-    );
+    ); */
 
-    // counterAI.inc();
-    // durationAI.set(duracion);
+    counterAI.inc();
+    durationAI.set(duracion);
+    
     return (
         <Link href={`/blog/vertexAI?country=${searchParams}`} className="post">
             <h3>Respuesta de Vertex AI</h3>
