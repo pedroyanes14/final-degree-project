96b8a795437929e9496e369b502133e71751e094
Pedro Yanes Garrido <pedro.yanes@edu.uah.es>
2024-06-22 17:15:41 +0200

    postAI page fixed

--- src/components/postAIPage.jsx
+++ src/components/postAIPage.jsx
@@ -1,12 +1,15 @@
 import ReactMarkdown from 'react-markdown';
+const { GoogleAuth } = require('google-auth-library');
 
 export default async function PostAIPage ({ searchParams }) {
+    let accessToken = "ya29.a0AXooCgv85laDFXjBL0OYEmVC1OEOUj4VyNzxwF5O_Iv_HTAi8bydINLqhC2DvljW7J6AmAWZhpyXeBO8CgrTZ57wPyDViXAdANiIl7McHkE-GdQp5zUP924H32jzUHFtbgQeD0zr6b40OV0CkAI2wplP5uQROLHjNS3itYUazIcaCgYKAZwSARMSFQHGX2MiOXLstGZnAKe7D67JAz99Dg0178";
+
 	const vertextResponse = await fetch(
         `https://us-central1-aiplatform.googleapis.com/v1/projects/final-degree-project-421721/locations/us-central1/publishers/google/models/text-bison:predict`,
         {
             method: "POST",
             headers: {
-            Authorization: "Bearer ya29.a0AXooCgtDRoL8QGLa1q7ptuCCTY0wvnFREi0zDmrKyh7OD0QwtrhUevk8hxRyBSrJcbJ1tvR6Cu6dJTU0DshOJmQdjGmrZi9V_hjK1_xFR7JyLIYE1Dy815_7nTnmoZCRIWMDzLLMJOUssuPex2Cb1CGoox3eeJjLs3X38gFTtAaCgYKAbwSARMSFQHGX2Miup7XY35e7B2DxPSUlCmw9w0177",
+            Authorization: `Bearer ${accessToken}`,
             "Content-Type": "application/json",
             },
             body: JSON.stringify({
@@ -17,6 +20,44 @@ export default async function PostAIPage ({ searchParams }) {
             })
         }
     );
+
+    if (vertextResponse.status !== 200) {
+        const auth = await new GoogleAuth({
+            scopes: "https://www.googleapis.com/auth/cloud-platform"
+        });
+        
+        const client = await auth.getClient();
+        accessToken = (await client.getAccessToken()).token;
+
+        const vertextResponse = await fetch(
+            `https://us-central1-aiplatform.googleapis.com/v1/projects/final-degree-project-421721/locations/us-central1/publishers/google/models/text-bison:predict`,
+            {
+                method: "POST",
+                headers: {
+                Authorization: `Bearer ${accessToken}`,
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                instances: [
+                    { content: `Make for me the best 2-day route in ${searchParams}` },
+                ],
+                parameters: { temperature: 0.2, maxOutputTokens: 1024, topP: 0.8, topK: 40 },
+                })
+            }
+        );
+
+        const data = await vertextResponse.json();
+
+        return (
+            <div className="single-blog-page">
+                <h2>{searchParams.toUpperCase()} ROAD TRIP</h2>
+                <div className="blog-post">
+                    <ReactMarkdown>{data.predictions[0].content}</ReactMarkdown>
+                </div>
+            </div>
+        );
+    }
+
     const data = await vertextResponse.json();
 
 	return (
